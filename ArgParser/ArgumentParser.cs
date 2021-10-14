ec9c80e6987c03fd4802d13ba4cd475dfcba6051
blyra <blyra05@gmail.com>
2021-10-14 19:27:42 -0400

    Initial Commit

---
+++ ArgParser/ArgumentParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace ArgParser
+{
+    public class ArgumentParser
+    {
+        public string Prog { get; set; }
+        public ArgumentParser()
+        {
+            Prog = Assembly.GetCallingAssembly().GetName().Name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"usage: {Prog} ");
+        }
+    }
+}
---
+++ PythonInterpreter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using ArgParser;
+
+namespace PythonInterpreter
+{
+    internal static class Program
+    {
+        private static string temp(string[] args)
+        {
+            string res = args.Aggregate("[", (current, s) => current + (s + ", "));
+            res += "]";
+            return res;
+        }
+        
+        public static void Main(string[] args)
+        {
+            ArgumentParser parser = new ArgumentParser();
+            
+            parser.Display();
+            
+            Console.WriteLine(temp(args));
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
